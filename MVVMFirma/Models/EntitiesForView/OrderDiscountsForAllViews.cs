786d3e7cd6fe6678c20676bb03571985cf0e08c1
Zabero <s.zaborski@gmail.com>
2024-12-14 21:15:51 +0100

    do sprawdzenie przez Marcina, problem z OrdersDiscountsView

---
+++ MVVMFirma/Models/EntitiesForView/OrderDiscountsForAllViews.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MVVMFirma.Models.EntitiesForView
+{
+    public class OrderDiscountsForAllViews
+    {
+        public int OrderDiscountID { get; set; }
+        public int OrderID { get; set; }
+        public int CodeID { get; set; }
+        public decimal DiscountAmount { get; set; }
+        public string Code { get; set; }
+        public Nullable<decimal> DiscountPercentage { get; set; }
+        public Nullable<DateTime> ExpiryDate { get; set; }
+        public Nullable<bool> IsActive { get; set; }
+
+
+
+    }
+}
--- MVVMFirma/ViewModels/OrderDiscountsViewModel.cs
+++ MVVMFirma/ViewModels/OrderDiscountsViewModel.cs
@@ -1,16 +1,34 @@
-﻿using System;
+﻿using MVVMFirma.Models.Entities;
+using MVVMFirma.Models.EntitiesForView;
+using MVVMFirma.ViewModels;
+using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Documents;
 
 namespace MVVMFirma.ViewModels
 {
-    public class OrderDiscountsViewModel : WorkspaceViewModel
+public class OrderDiscountsViewModel : GetDataFromTable<OrderDiscountsForAllViews>
+{
+    public OrderDiscountsViewModel() : base("Rabaty") { }
+
+    public override void Load()
     {
-        public OrderDiscountsViewModel()
+        List = new ObservableCollection<OrderDiscountsForAllViews>(
+        _voucherShopEntities.OrderDiscounts.Select(c => new OrderDiscountsForAllViews
         {
-            base.DisplayName = "Rabaty";
-        }
+            OrderDiscountID = c.OrderDiscountID,
+            OrderID = c.OrderID,
+            DiscountAmount = c.DiscountAmount,
+            CodeID = c.DiscountCodes.CodeID,
+            Code = c.DiscountCodes.Code,
+            DiscountPercentage =c.DiscountCodes.DiscountPercentage,
+            ExpiryDate = c.DiscountCodes.ExpiryDate,
+            IsActive = c.DiscountCodes.IsActive,
+        }));
     }
+}
 }
